13a7781f3c8d8ca750163c57e899c94e6014de8b
kisstupid <tp.nam@tego.global>
2022-03-15 09:02:28 +0700

    refactor: update image container styles

--- src/plugins/ImageDrop.ts
+++ src/plugins/ImageDrop.ts
@@ -7,20 +7,15 @@ export class ImageDrop extends BaseImageDrop {
     insert(dataUrl: string) {
         const self: any = this
         const imageElm = document.createElement('img')
-
+        imageElm.style.height = '80px'
+        imageElm.style.width = '80px'
         imageElm.src = dataUrl
 
         let imageContainer = self.quill.container.querySelector(".image-container")
         if (!imageContainer) {
             imageContainer = document.createElement('div')
             imageContainer.classList = ['image-container']
-            imageContainer.style.position = "absolute"
-            imageContainer.style.bottom = "0"
-            imageContainer.style.left = "0"
-            imageContainer.style.right = "0"
-            imageContainer.style.height = "100px"
 
-            self.quill.container.style.paddingBottom = "100px"
             self.quill.container.appendChild(imageContainer)
         }
 
